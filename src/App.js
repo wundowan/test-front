49ac2444deaf2bd0e4a1870bf6d71ffb7ee25829
wundowan <wundowan@protonmail.com>
2021-03-22 15:17:07 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import ClippedDrawer from './components/AppBar/ClippedDrawer';
+
+function App() {
+  return (
+    <div className="App">
+      <ClippedDrawer/>
+      <header className="App-header">
+        AOB Property Database   
+      </header>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
